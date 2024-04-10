9ca93c3e76c34f7cbacf0f42add0ded4aa249547
Iure Santiago <iuresantiag016@gmail.com>
2024-04-10 12:02:49 -0300

    correção exercicios para casa dia-02

--- dia02/exercicios-casa/exB.py
+++ dia02/exercicios-casa/exB.py
@@ -1,17 +1,30 @@
 sorvete = input("escolha um tipo de sorvete entre: casquinha, cascão ou cestinha! ")
-
 sabor = input("agora escolha um sabor entre os sabores napolitanos! ")
-
 cob = input("e por final escolha uma cobertura entre: caramelo, chocolate, morango ou sem cobertura! ")
 
-valorFinal = float
-valorParcial = float
+valor = 0
 
+#parte do tipo de sorvete
 if sorvete == "casquinha":
-    valorParcial = 1.5
+    valor = valor + 1.00
 
 elif sorvete == "cascao":
-    valorParcial = 2.5
+    valor = valor +  2.5
 
+elif sorvete == "cestinha":
+    valor = valor +  4.0
 
-### há de se terminar...
+else:
+    print("opção inválida de sorvete")
+
+#parte da cobertura
+if cob == "caramelo" or "chocolate" or "morango":
+    valor += 1.5
+
+elif cob == '':
+    pass
+
+else:
+    print("escolha uma cobertura válida!")
+
+print("seu sorvete", sorvete, "de", sabor, "com cobertura de", cob, "custará", valor)
